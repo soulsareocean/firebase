7f3a8e075cf0b19a67517c26de808b374dc24bf7
soulsareocean <34334162+soulsareocean@users.noreply.github.com>
2017-12-07 15:34:55 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase.js');
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase.js');
 
 var config = {
     apiKey: "AIzaSyDtfDX6ZbWHalyzqpX98oPa0tJYybNS0p8",
